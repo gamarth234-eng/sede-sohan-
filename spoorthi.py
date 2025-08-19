a84d1669cd0c606c707109e85c2cd84cccace815
samarth123 <gamarth234@gmail.com>
2025-08-19 12:19:13 +0530

    Add files via upload

---
+++ spoorthi.py
@@ -0,0 +1,10 @@
+username=input("enter the username:")
+password=input("enter the password:")
+if username=="admin"and password=="gptk@123":
+ print("welcome admin")
+ print("good morning,have a great day.")
+elif username=="ramu"and password=="ramu@123":
+ print("welcome ramu")
+ print("good morning,have a great day.")
+else:
+ print("invalid user name or password")
